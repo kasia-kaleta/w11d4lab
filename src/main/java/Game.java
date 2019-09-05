88a7ae19592720c3211e8730b41cd95adcafe35f
Josh Gibson <flash50k@gmail.com>
2019-09-05 14:50:02 +0100

    deal cards

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,28 @@
 public class Game {
+    public static void main(String[] args) {
+
+        Deck deck = new Deck();
+        deck.initializeDeck();
+        deck.shuffleDeck();
+
+        Card p1card;
+        Card p2card;
+        p1card = deck.dealCard();
+        p2card = deck.dealCard();
+
+        String result;
+        if (p1card.getValueFromEnum() > p2card.getValueFromEnum()){
+           result =  "P1 Wins!";
+        }
+        else if (p2card.getValueFromEnum() > p1card.getValueFromEnum()){
+            result =  "P2 Wins!";
+        }
+        else {result = "Draw!";}
+
+
+        System.out.println("Welcome to the silly game!");
+        System.out.println("Player 1 is dealt... " + p1card.getRank().toString() + " of " + p1card.getSuit().toString());
+        System.out.println("Player 2 is dealt... " + p2card.getRank().toString() + " of " + p2card.getSuit().toString());
+        System.out.println("The result is... " + result);
+    }
 }
