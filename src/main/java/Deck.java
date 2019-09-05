d40420a1ce7483c61765b0ce90746279797b1f0b
Josh Gibson <flash50k@gmail.com>
2019-09-05 13:09:05 +0100

    set up

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        cards = new ArrayList<Card>();
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public int getCardsSize() {
+        return this.cards.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck newDeck;
+
+    @Before
+    public void setUp(){
+        newDeck = new Deck();
+    }
+
+    @Test
+    public void canGetCardsSize(){
+        assertEquals(0, newDeck.getCardsSize());
+    }
+
+
+    @Test
+    public void canGetCards(){
+        assertEquals(0, newDeck.getCardsSize());
+    }
 }
